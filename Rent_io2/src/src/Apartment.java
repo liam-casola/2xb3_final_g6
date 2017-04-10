555ccc3e06c93a636e332041614ba060d3771081
Liam Casola <liam.casola@gmail.com>
2017-04-10 18:34:55 -0400

    Added comments

--- Rent_io2/src/src/Apartment.java
+++ Rent_io2/src/src/Apartment.java
@@ -7,6 +7,7 @@ public class Apartment {
 	private String unit;
 	private double value;
 	
+	//creates a new apartment with the given properties
 	public Apartment(
 					int year,  
 					int population,
@@ -21,22 +22,28 @@ public class Apartment {
 		this.value = value;
 	}
 	
+	//returns the year associated with the apartment
 	public int getYear() {
 		return year;
 	}
 	
+	//returns the population associated with the apartment
+	//this value is not going to be used
 	public int getPopulation() {
 		return population;
 	}
 	
+	//returns the type of structure the apartment resides in
 	public String getStructure() {
 		return structure;
 	}
 	
+	//retunrs the size of the unit the apartment is
 	public String getUnit() {
 		return unit;
 	}
 	
+	//reutnrs the average cost of the apartment per month
 	public double getValue() {
 		return value;
 	}
