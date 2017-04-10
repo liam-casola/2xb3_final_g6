622ff0812f4e0ca715088d99f819cf6187f3d95f
Liam Casola <liam.casola@gmail.com>
2017-04-10 18:32:14 -0400

    added comments

--- Rent_io2/src/src/Edge.java
+++ Rent_io2/src/src/Edge.java
@@ -5,47 +5,28 @@ public class Edge implements Comparable<Edge> {
     private final int v;
     private final int w;
     private final double weight;
-
+    
+    //createds a new weighted edge
     public Edge(int v, int w, double weight) {
         this.v = v;
         this.w = w;
         this.weight = weight;
     }
-
+    
+    //retunrs the weight of the edge
     public double weight() {
         return weight;
     }
-
+    
+    //returns one of the vertex's of the edge
     public int either() {
         return v;
     }
-
-    /**
-     * Returns the endpoint of this edge that is different from the given vertex.
-     *
-     * @param  vertex one endpoint of this edge
-     * @return the other endpoint of this edge
-     * @throws IllegalArgumentException if the vertex is not one of the
-     *         endpoints of this edge
-     */
+    
+    //returns the value of the vertex opposite to the vertex given.
     public int other(int vertex) {
         if      (vertex == v) return w;
         else if (vertex == w) return v;
         else throw new IllegalArgumentException("Illegal endpoint");
-    }
-
-    /**
-     * Compares two edges by weight.
-     * Note that {@code compareTo()} is not consistent with {@code equals()},
-     * which uses the reference equality implementation inherited from {@code Object}.
-     *
-     * @param  that the other edge
-     * @return a negative integer, zero, or positive integer depending on whether
-     *         the weight of this is less than, equal to, or greater than the
-     *         argument edge
-     */
-    @Override
-    public int compareTo(Edge that) {
-        return Double.compare(this.weight, that.weight);
     }
 }
