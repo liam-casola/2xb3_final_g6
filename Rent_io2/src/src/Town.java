79cfecfac482bf347203b6e4e162cf103479d9cd
Liam Casola <liam.casola@gmail.com>
2017-04-10 18:55:45 -0400

    Update Town.java

--- Rent_io2/src/src/Town.java
+++ Rent_io2/src/src/Town.java
@@ -6,6 +6,7 @@ public class Town {
 	private double longitude;
 	private Apartment[] aptsList;
 	
+	//initalize a town with the 4 values it has
 	public Town(String location, double latitude, double longitude, Apartment[] aptsList) {
 		this.location = location;
 		this.latitude = latitude;
@@ -13,32 +14,38 @@ public class Town {
 		this.aptsList = aptsList;
 	}
 	
+	//get the name of the town
 	public String getLocation() {
 		return location;
 	}
 	
+	//get the latitude of the town
 	public double getLatitude() {
 		return latitude;
 	}
 	
+	//get the longitude of the town
 	public double getLongitude() {
 		return longitude;
 	}
 	
+	//get the array of relevent apartments to this town
 	public Apartment[] getAptsList() {
 		return aptsList;
 	}
 	
-    // distance calculation
-    public double distanceTo(Town t){
-    	final double R = 6372.8; // In kilometers
-    	double dLat = Math.toRadians(t.getLatitude() - this.getLatitude());
-    	double dLon = Math.toRadians(t.getLongitude() - this.getLongitude());
-    	double lat1 = Math.toRadians(this.getLatitude());
-    	double lat2 = Math.toRadians(t.getLatitude());
-    	double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
-    	double c = 2 * Math.asin(Math.sqrt(a));
-    	double distance = R * c;
-		return distance;
+    // distance calculation from this town to another
+	public double distanceTo(Town t) {
+		//becasue latitude and longitude are not the representation of coordinates on a 2D grid, special calculations
+		//need to be made to determine the distance between the two points
+		final double R = 6372.8; // In kilometers
+		double dLat = Math.toRadians(t.getLatitude() - this.getLatitude());
+	    	double dLon = Math.toRadians(t.getLongitude() - this.getLongitude());
+	    	double lat1 = Math.toRadians(this.getLatitude());
+	    	double lat2 = Math.toRadians(t.getLatitude());
+	    	double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+	    	double c = 2 * Math.asin(Math.sqrt(a));
+	    	double distance = R * c;
+	    	return distance;
     }
 }
