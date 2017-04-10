e9f9b1162b28f11ef4b533354016dea7cdc1f9c7
Liam Casola <liam.casola@gmail.com>
2017-04-10 13:38:26 -0400

    Update ProcessRequest.java

--- Rent_io2/src/src/ProcessRequest.java
+++ Rent_io2/src/src/ProcessRequest.java
@@ -3,36 +3,41 @@ package src;
 import java.util.ArrayList;
 
 public class ProcessRequest {
-	public static void priceTrend(Town t) {
-		//determine the change in price every year for an apartment
-		ArrayList<Apartment> al = new ArrayList<Apartment>();
-		ArrayList<Double> cv = new ArrayList<Double>();
-		for(int i = 1; i < t.getAptsList().length; i++) {
-			if (t.getAptsList()[i - 1].getYear() < t.getAptsList()[i].getYear() && t.getAptsList()[i - 1].getValue() > 0 && t.getAptsList()[i].getValue() > 0) {
-				al.add(t.getAptsList()[i - 1]);
-			} else {
-				al.add(t.getAptsList()[i - 1]);
-				Apartment[] a = al.toArray(new Apartment[al.size()]);
-				cv.add(averagePriceChange(a));
-				System.out.println(cv.get(0).toString());
-				al.clear();
-			}
-			
-		}
-	}
 	
-	public static double averagePriceChange(Apartment[] a) {
-		double changeValue = 0;
-		for (int i = 1; i < a.length; i++) {
-			changeValue += a[i].getValue() - a[i - 1].getValue();
-		}
-		changeValue = changeValue / (a.length - 1);
-		return changeValue;
-	}
-	
+	//prints the price of all structures and unit types in the selected town
 	public static void currentPrices(Town t) {
 		for (int i = 0; i < t.getAptsList().length; i++) {
 			if (t.getAptsList()[i].getYear() == 2016 && t.getAptsList()[i].getValue() > 0) {
+				System.out.println("--Building Type--\n" + t.getAptsList()[i].getStructure());
+				System.out.println("--Unit Size--\n" + t.getAptsList()[i].getUnit());
+				System.out.println("--Average Unit Price--\n$" + t.getAptsList()[i].getValue() + "/m");
+				System.out.println();
+			}
+		}
+	}
+	
+	public static void priceHistory(Town t) {
+		double change = 0;
+		double counter = 0;
+		
+		for (int i = 0; i < t.getAptsList().length; i++) {
+			if (i < t.getAptsList().length - 1 && t.getAptsList()[i].getValue() > 0) {
+				counter++;
+				if (t.getAptsList()[i].getYear() < t.getAptsList()[i + 1].getYear()) {
+					change = t.getAptsList()[i + 1].getValue() - t.getAptsList()[i].getValue();
+				} else {
+					System.out.print("$" + ((double)(int)(change / counter * 100)) / 100);
+					System.out.println("\tis the average price increase per year for " + t.getAptsList()[i].getStructure() + " with " + t.getAptsList()[i].getUnit());
+					change = 0;
+					counter = 0;
+				}
+			}
+		}
+	}
+	
+	public static void priceLimit(Town t, double r) {
+		for (int i = 0; i < t.getAptsList().length; i++) {
+			if (t.getAptsList()[i].getYear() == 2016 && t.getAptsList()[i].getValue() > 0 && t.getAptsList()[i].getValue() < r) {
 				System.out.println("--Building Type--\n" + t.getAptsList()[i].getStructure());
 				System.out.println("--Unit Size--\n" + t.getAptsList()[i].getUnit());
 				System.out.println("--Average Unit Price--\n$" + t.getAptsList()[i].getValue() + "/m");
